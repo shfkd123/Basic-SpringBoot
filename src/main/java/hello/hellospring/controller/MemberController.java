8e45e847353f5830085b81bd84f203bbbdd661e2
shfkd123 <shfkd123@gmail.com>
2022-09-07 23:05:10 +0900

    회원가입, 회원조회 예제

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberSerivce;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -21,5 +27,28 @@ public class MemberController {
     @Autowired // 생성자 주입 - 의존관계가 실행중에 동적으로 변하는 경우는 거의 없으므로 생성자 주입을 거의 사용한다.
     public MemberController(MemberSerivce memberSerivce) {
         this.memberSerivce = memberSerivce;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberSerivce.join(member);
+
+        //System.out.println("member : " +  member.getName()) ;
+
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model){
+        List<Member> memList = memberSerivce.findMembers();
+        model.addAttribute("members", memList);
+        return "/members/memberList";
     }
 }
